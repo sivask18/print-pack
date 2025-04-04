0fbd411b7e06dad71ca5678580537c672b3c9333
Sivakumar M <sivakumar5810m@outlook.com>
2025-04-04 22:47:52 +0530

    Updated Git code

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+
+// this is the main component of the project
+function App() {
+  console.log('App component loaded');
+  
+return(
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </header>
+    </div>
+  )
+}
+export default App
